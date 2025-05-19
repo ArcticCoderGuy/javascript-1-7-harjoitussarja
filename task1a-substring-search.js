d8e6975d644723b2f6e8330eb2c5217ffc3784ca
Your Name <you@example.com>
2025-05-19 23:48:57 +0300

    1a tehtävä tehty ja testattu toimivaksi

--- task1a-substring-search.js
+++ task1a-substring-search.js
@@ -1,1 +1,16 @@
 // Task 1a: Etsi 'paja' merkkijonosta
+
+const mystinenSana = "asuntokionapajaa";
+let uusiMuutuja;
+
+if(mystinenSana.indexOf("paja") !== -1) {
+  uusiMuutuja = "paja";
+
+  console.log("Origin string", mystinenSana);
+  console.log("Substring found:", uusiMuutuja);
+} else {
+  console.log("Sanaa ei löytynyt"), mystinenSana;
+    console.log("Substring not found"), uusiMuutuja;
+} 
+    
+     
